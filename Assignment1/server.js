f3c678453930053de53957a076118ad4e5c18cf9
JoshHeida <joshheida@gmail.com>
2022-06-05 15:50:16 -0400

    express

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -3,29 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const fs_1 = __importDefault(require("fs"));
-const mime_types_1 = __importDefault(require("mime-types"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-let lookup = mime_types_1.default.lookup;
-const server = http_1.default.createServer(function (req, res) {
-    let path = req.url;
-    if (path == "/" || path == "/home") {
-        path = "/index.html";
-    }
-    let mime_type = lookup(path.substring(1));
-    fs_1.default.readFile(__dirname + path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            console.log(err.message);
-            res.end(err.message);
-        }
-        res.setHeader("X-content-type-options", "nosniff");
-        res.writeHead(200, { 'Content-Type:': mime_type });
-        return res.end(data);
-    });
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-server.listen(port, function () {
-    console.log(`Server running at port ${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
 //# sourceMappingURL=server.js.map
--- Assignment1/server.ts
+++ Assignment1/server.ts
@@ -1,32 +1,13 @@
 //Josh Heida 300644912
 
-import http from 'http';
-import fs from 'fs';
-import mime from 'mime-types';
-const port = 3000;
+import express from 'express'
+const app = express()
+const port = 3000
 
-let lookup = mime.lookup;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const server =  http.createServer(function(req,res){
-    let path = req.url as string;
-
-    if (path== "/" || path == "/home"){
-        path="/index.html";
-    }
-    let mime_type = lookup(path.substring(1)) as string;
-
-    fs.readFile(__dirname + path, function(err, data){
-        if (err){
-            res.writeHead(404);
-            console.log(err.message);
-            res.end(err.message);
-        }
-        res.setHeader("X-content-type-options","nosniff")
-        res.writeHead(200,{'Content-Type:': mime_type});
-        return res.end(data);
-    })
-
-});
-server.listen(port,function(){
-    console.log(`Server running at port ${port}`)
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
