7ba24fd1e2f5ab32ac099a9d17ea35f21d32397f
JoshHeida <joshheida@gmail.com>
2022-06-05 16:58:46 -0400

    express + ts working

--- Assignment1/server.ts
+++ Assignment1/server.ts
@@ -1,13 +1,88 @@
-//Josh Heida 300644912
+#!/usr/bin/env node
 
-import express from 'express'
-const app = express()
-const port = 3000
+/**
+ * Module dependencies.
+ */
+import app from './app';
+import debug from 'debug';
+debug('assignment1:server');
+import http from 'http';
+import { HttpError } from 'http-errors';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val:string) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+
+    return val;
+  }
+
+  if (port >= 0) {
+
+    return port;
+  }
+
+  return false;
+}
+
+
+
+function onError(error:HttpError) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  let addr = server.address() as string;
+  let bind = 'pipe ' + addr;
+  debug('Listening on ' + bind);
+}
